a85a2e28d24cbe38f96d42730a7cf1708572f32b
KAUSTUBH  SEN <kaustubhsen2707@gmail.com>
2024-12-13 13:38:24 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    phone_no = models.CharField(max_length=15, blank=True, null=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
